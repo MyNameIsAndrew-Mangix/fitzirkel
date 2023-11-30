965c5b8ab0e6d6687c4b6320490508edb9c76ee1
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2023-11-30 16:16:01 -0500

    +nextui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { NextUIProvider } from '@nextui-org/react';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <NextUIProvider>
+            <main className="light text-foreground bg-background">
+                <App />
+            </main>
+        </NextUIProvider>
+    </React.StrictMode>
 );
